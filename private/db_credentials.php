eee33fc425d23870d5a50d53d930b968bdd44d33
codebyomar <omarmohammed580@gmail.com>
2018-10-17 16:39:42 +0100

    db connection

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,10 +1,10 @@
 <?php
 
 if(isset($_ENV["PHP_ENV"])) {
-  define("DB_SERVER", $_ENV["DB_SERVER"]);
-  define("DB_USER", $_ENV["DB_USER"]);
-  define("DB_PASS", $_ENV["DB_PASS"]);
-  define("DB_NAME", $_ENV["DB_NAME"]);
+  define("DB_SERVER", $_SERVER["DB_SERVER"]);
+  define("DB_USER", $_SERVER["DB_USER"]);
+  define("DB_PASS", $_SERVER["DB_PASS"]);
+  define("DB_NAME", $_SERVER["DB_NAME"]);
 }else {
   define("DB_SERVER", "127.0.0.1");
   define("DB_USER", "root");
