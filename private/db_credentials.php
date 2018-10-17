f1da3a8a6fe1b42bd6771c3981f3b6ba50e60c6f
codebyomar <omarmohammed580@gmail.com>
2018-10-17 16:59:20 +0100

    var initialization

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,12 +1,15 @@
 <?php
 if(isset($_ENV["PHP_ENV"])) {
-  print "in: ";
-  define("DB_SERVER", getenv("DB_SERVER"));
-  define("DB_USER", getenv("DB_USER"));
-  define("DB_PASS", getenv("DB_PASS"));
-  define("DB_NAME", getenv("DB_NAME"));
+  $dbserver = getenv("DB_SERVER");
+  $dbuser = getenv("DB_USER");
+  $dbpass = getenv("DB_PASS");
+  $dbname = getenv("DB_NAME");
+
+  define("DB_SERVER", $dbserver);
+  define("DB_USER", $dbuser);
+  define("DB_PASS", $dbpass);
+  define("DB_NAME", $dbname);
 }else {
-  print "out";
   define("DB_SERVER", "127.0.0.1");
   define("DB_USER", "root");
   define("DB_PASS", "");
