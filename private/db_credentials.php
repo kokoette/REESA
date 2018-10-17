42a84fe32c983d8b2a4e736e2f2c89a708cfddc9
codebyomar <omarmohammed580@gmail.com>
2018-10-17 16:55:51 +0100

    file naming conventions

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,11 +1,12 @@
 <?php
-
-if(getenv("PHP_ENV") !== null) {
+if(isset($_ENV["PHP_ENV"])) {
+  print "in: ";
   define("DB_SERVER", getenv("DB_SERVER"));
   define("DB_USER", getenv("DB_USER"));
   define("DB_PASS", getenv("DB_PASS"));
   define("DB_NAME", getenv("DB_NAME"));
 }else {
+  print "out";
   define("DB_SERVER", "127.0.0.1");
   define("DB_USER", "root");
   define("DB_PASS", "");
