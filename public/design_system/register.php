28c585656775903d040bce5466934fb487a984c3
jamez <james.i@comsoftltd.com>
2018-10-20 19:51:40 +0100

    UI Corrections

--- public/design_system/register.php
+++ public/design_system/register.php
@@ -80,8 +80,8 @@ if(is_post_request()) {
                 </div>
 
             <ul class="navbar-nav ml-auto">
-              <!-- <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
-              <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+              <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+              <!-- <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
               <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
               <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li> -->
               <li class="nav-item"><a href="faq.php" class="nav-link">FAQs</a></li>
